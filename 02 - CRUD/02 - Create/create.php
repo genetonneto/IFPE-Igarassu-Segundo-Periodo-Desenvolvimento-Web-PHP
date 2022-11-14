35a7cf6f456460f2fea50296294ea2faa31c0490
Geneton Neto <gasn1@discente.ifpe.edu.br>
2022-11-14 15:17:08 -0300

    atualizando html e css dos diretorios

--- 02 - CRUD/02 - Create/create.php
+++ 02 - CRUD/02 - Create/create.php
@@ -1,16 +1,48 @@
-<?php 
+<?php
 
-$nome = $_GET['nome'];
-$idade = $_GET['age'];
-$estado = $_GET['estado'];
+$dia = $_GET['dia'];
+$treino01 = $_GET['treino01'];
+$treino02 = $_GET['treino02'];
+$treino03 = $_GET['treino03'];
 
-$file = fopen('pessoas.csv', 'a');
-fwrite($file, "$nome, $idade, $estado\n");
+$file = fopen('treino.csv', 'a');
+fwrite($file, "$dia, $treino01, $treino02, $treino03\n");
 
 ?>
 
-<h2>Valores inseridos</h2>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create</title>
+</head>
+<style>
+    * {
+        font-family: sans-serif;
+    }
 
-<p>Nome: <?= $nome ?></p>
-<p>Idade: <?= $idade ?></p>
-<p>Estado: <?= $estado ?></p>
+    table, th, td {
+        text-align: center;
+        border: 2px solid black;
+    }
+
+    p {
+        text-align: center;
+        border: 2px solid black;
+    }
+</style>
+<body>
+    <table>
+        <th colspan="100px">
+            <h2>Treino Adicionado</h2>
+            <p>Dia: <?= $dia ?></p>
+            <p>Treino:<?= $treino01 ?></p>
+            <p>Treino:<?= $treino02 ?></p>
+            <p>Treino:<?= $treino03 ?></p>
+        </th>
+    </table>
+    <a href="table.php">Voltar</a>
+</body>
+</html>
--- 02 - CRUD/02 - Create/table.php
+++ 02 - CRUD/02 - Create/table.php
@@ -1,4 +1,4 @@
-<?php $file = fopen('pessoas.csv', 'r'); ?>
+<?php $file = fopen('treino.csv', 'r') ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -6,19 +6,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Ficha de Treino</title>
 </head>
+<style>
+    * { font-family: sans-serif; }
+
+    table,th, td {
+        text-align: center;
+        border: 2px solid black;
+    }  
+
+    th { background-color: #F2B705; } 
+
+    td { background-color: #03A63C; }
+</style>
 <body>
-    <h1>Dados</h1>
     <table>
-    <?php while($linha = fgets($file)):  ?>   
+        <th colspan="100px">Ficha de Treino</th>
         <tr>
-            <?php $partes = explode(',', $linha) ?>
-            <?php foreach($partes as $dado):  ?>
-            <td><?= $dado ?></td>
-            <?php endforeach ?>
+            <td style="background-color:#F28705;">Dia</td>
+            <td style="background-color:#F28705;" colspan="100px">Treino</td>
+            <?php while($linha = fgets($file)): ?>
+                <tr>
+                    <?php $partes = explode(',', $linha) ?>
+                    <?php foreach($partes as $dados): ?>
+                        <td><?= $dados ?></td>
+                    <?php endforeach ?>   
+                </tr>
+            <?php endwhile ?>
         </tr>
-       <?php endwhile ?>
     </table>
     <hr>
     <a href="form.html">Adicionar</a>
